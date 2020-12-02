f35e1d1c706d1ac3ed4d7275571a37a3b551e3cb
Phil Krull <15981676+phil-krull@users.noreply.github.com>
2020-12-02 08:40:59 -0600

    adding w1d3 algos

---
+++ algorithms/weekOne/dayThree.js
@@ -0,0 +1,9 @@
+// Remove Duplicates
+// ------------------------------------------------------------------
+// Remove duplicates from an array. Do not alter the original array; return a new one, keeping results ‘stable’ (retain original order). Given [1, 2, 1, 3, 4, 2], return a new array [1, 2, 3, 4].
+
+// Second: Work ‘in-place’ in given array. Alter order if needed (stability is not required). Ex.: [1,2,1,3,4,2] could become [1,2,4,3].
+
+// Third: Make it in-place and stable.
+
+// Fourth: Can you make this faster by eliminating any second inner loop?
