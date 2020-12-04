bc64f4688e11c744284d39b18b28f0e61fd6c98b
Phil Krull <15981676+phil-krull@users.noreply.github.com>
2020-12-04 17:19:00 -0600

    adding controllers and templates

--- springdemo/src/main/java/com/pkrull/Controllers/HomeController.java
+++ springdemo/src/main/java/com/pkrull/Controllers/HomeController.java
@@ -1,11 +1,13 @@
 package com.pkrull.Controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/demo")
 public class HomeController {
 	@RequestMapping("")
@@ -20,6 +22,19 @@ public class HomeController {
 	
 	@RequestMapping("/location/{location}")
 	public String location(@PathVariable("location") String loc) {
+		if(loc.equals("tulsa")) {
+			System.out.println("yes");
+		} else {
+			System.out.println("no");
+		}
 		return "Location: " + loc;
+	}
+	
+	@RequestMapping("/show/{location}")
+	public String show(Model model, @PathVariable("location")String loc) {
+		String name = "Phil";
+		model.addAttribute("userName", name);
+		model.addAttribute("useLocation", loc);
+		return "show.jsp";
 	}
 }
---
+++ springdemo/src/main/webapp/WEB-INF/show.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    <%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Show</title>
+</head>
+<body>
+	<h1>On show page!</h1>
+	<p><c:out value="${2+2}"/></p>
+	<h2>Name: <c:out value="${userName}"/> From: <c:out value="${useLocation}"/></h2>
+</body>
+</html>
