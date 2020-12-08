69a60352280aeaecda7bedf25c05b58de6423fea
Phil Krull <15981676+phil-krull@users.noreply.github.com>
2020-12-08 08:45:18 -0600

    adding w2d2 algo challenge

---
+++ algorithms/weekTwo/dayTwo.js
@@ -0,0 +1,3 @@
+// KthLast
+// ----------------------------------------------------------------------
+// Given k, return the value that is ‘k’ nodes from the list’s end. If given (list,1), return the list’s last value. If given (list,4), return the value at the node that has exactly 3 nodes following it.
