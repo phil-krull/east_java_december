26b9810cd036f39694ab7af0f3e27e0d08d08893
Phil Krull <15981676+phil-krull@users.noreply.github.com>
2020-12-09 17:13:56 -0600

    adding create and read with templates

--- book_demo/src/main/java/com/pkrull/Controllers/BooksApi.java
+++ book_demo/src/main/java/com/pkrull/Controllers/BooksApi.java
@@ -2,24 +2,32 @@ package com.pkrull.Controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.pkrull.Models.Book;
 import com.pkrull.Services.BookService;
 
-@RestController
+@Controller
 public class BooksApi {
     private final BookService bookService;
     public BooksApi(BookService bookService){
         this.bookService = bookService;
     }
-    @RequestMapping("/api/books")
-    public List<Book> index() {
-        return bookService.allBooks();
+    @RequestMapping("/books")
+    public String index(Model model) {
+        List<Book> allBooks = bookService.allBooks();
+        model.addAttribute("books", allBooks);
+        
+        return "index.jsp";
     }
     
     @RequestMapping(value="/api/books", method=RequestMethod.POST)
@@ -28,9 +36,24 @@ public class BooksApi {
         return bookService.createBook(book);
     }
     
+    @RequestMapping(value="/books", method=RequestMethod.POST)
+    public String create(@Valid @ModelAttribute("book") Book book, BindingResult result) {
+        if (result.hasErrors()) {
+            return "new.jsp";
+        } else {
+            bookService.createBook(book);
+            return "redirect:/books";
+        }
+    }
+    
     @RequestMapping("/api/books/{id}")
     public Book show(@PathVariable("id") Long id) {
         Book book = bookService.findBook(id);
         return book;
+    }
+    
+    @RequestMapping("/books/new")
+    public String newBook(@ModelAttribute("book") Book book) {
+        return "new.jsp";
     }
 }
